037427be96fe7ecb6916f539354e824a4bffb3d7
Ti-Ok123 <152749340+Ti-Ok123@users.noreply.github.com>
2025-01-31 09:31:05 +0100

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -14,6 +14,34 @@ public class BubbleSort<ContentType extends ComparableContent<ContentType>> {
     }
 
     public static <ContentType> void swap(List<ContentType> list, ContentType item1, ContentType item2) {
+        if (list.isEmpty() || item1.equals(item2)) {
+            return; // No need to swap if the list is empty or both items are the samee
+        }
 
+        list.toFirst();
+        boolean foundItem1 = false, foundItem2 = false;
+
+        // Find both elements
+        while (list.hasAccess()) {
+            if (list.getContent().equals(item1)) {
+                foundItem1 = true;
+            } else if (list.getContent().equals(item2)) {
+                foundItem2 = true;
+            }
+            list.next();
+        }
+
+        // If both elements are found, swap them
+        if (foundItem1 && foundItem2) {
+            list.toFirst();
+            while (list.hasAccess()) {
+                if (list.getContent().equals(item1)) {
+                    list.setContent(item2);
+                } else if (list.getContent().equals(item2)) {
+                    list.setContent(item1);
+                }
+                list.next();
+            }
+        }
     }
 }
